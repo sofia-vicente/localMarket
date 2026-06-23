40786a22c7c1764c28cbff2584501e185489bf17
Sofia <vicente270510@gmail.com>
2026-06-23 12:38:59 -0300

    Add files via upload

--- src/Carrinho/script.js
+++ src/Carrinho/script.js
@@ -1,18 +1,60 @@
 $(document).ready(function() {
     const carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
 
-    const listaElement = $("#lsta")
+    const listElement = $('#lista')
 
-    const totalElent = $("#total")
+    const totalElement = $("#total")
 
     function exibirCarrinho(){
-        listaElement.empty()
+        listElement.empty()
         let totalPreco = 0
 
         $.each(carrinho, function(index, item){
-            const listItem
+            const listItem = $("<li>").text(`${item.desc} - Preço: $${item.preco.toFixed(2)}`)
+
+            const removeButton = $("<button>").text("❌").css("margin-left", "10px").click(function()
+            {
+                removerItem(index)
+            })
+
+            listItem.append(removeButton)
+            listElement.append(listItem)
+
+            totalPreco += item.preco
         })
+        totalElement.text(`Total $${totalPreco.toFixed(2)}`)
     }
 
-
+    function removerItem(index){
+        carrinho.splice(index, 1)
+        localStorage.setItem("carrinho", JSON.stringify(carrinho))
+        exibirCarrinho()
+    }
+    exibirCarrinho()
 })
+function gerar(){
+    const listElement = document.getElementById("lista")
+    const totalElement = document.getElementById("total")
+    const listaClone = listElement.cloneNode(true)
+    $(listaClone).find("button").remove()
+    const listaHtml = listaClone.innerHTML
+    const totalHtml = totalElement.innerHTML
+    const conteudoHTML = `
+        <html>
+            <head>
+                <meta charset="UTF=8">
+            </head>
+            <body>
+                <h1>PEDIDO CONFIRMADO</h1>
+                <h3>Agradeçemos sua compra e sua preferencia</h3>
+                <br>
+                ${listaHtml}
+                <br>
+                <br>
+                ${totalHtml}
+            </body>
+            </html>
+    `
+    const blob = new blob ([conteudoHTML], {type: ""}) 
+    const limk = ([])
+}
